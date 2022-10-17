b8049fa734c1c6b867862d76554f81b4dc1bb9bc
Kbalod <kbalod@yandex.ru>
2022-10-17 20:08:26 +0300

    поправил банер

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -18,7 +18,7 @@ function Banner({promo}:BannerReview) : JSX.Element {
         <span className="banner__message">Новинка!</span>
         <span className="title title--h1">{name}</span>
         <span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span>
-        <Link className="btn" to={`${AppRoute.Camera}/${id}`}>Подробнее</Link>
+        <Link className="btn" to={`${AppRoute.Camera}${id}`}>Подробнее</Link>
       </p>
     </div>
   );
