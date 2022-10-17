0a95dfdd6613c7a365e81d9dd631546bd5d88d78
Kbalod <kbalod@yandex.ru>
2022-10-17 19:05:21 +0300

    добавил FokusTrap

--- src/components/modal-review/modal-review.tsx
+++ src/components/modal-review/modal-review.tsx
@@ -4,6 +4,7 @@ import { useAppDispatch, useAppSelector } from '../../hooks';
 import { fetchReviewsAction, postReviewAction } from '../../store/action';
 import { getNewCommentError } from '../../store/errors-data/selectors';
 import { PostReview } from '../../types/data';
+import FocusTrap from 'focus-trap-react';
 
 type Modal = {
   id: string,
@@ -59,99 +60,101 @@ function ModalReview({id,setOpenModal,setSuccessPost} : Modal) : JSX.Element {
   document.addEventListener('keydown', handlePressEsc);
   document.body.classList.add('scroll-lock');
   return (
-    <div className="modal is-active">
-      <div className="modal__wrapper" onKeyDown={()=>handleCloseModal}>
-        <div className="modal__overlay" onClick={handleCloseModal}></div>
-        <div className="modal__content">
-          <p className="title title--h4">Оставить отзыв</p>
-          <div className="form-review">
-            <form method="post" onSubmit={handleFormSubmit}>
-              <div className="form-review__rate">
-                <fieldset className={`rate form-review__item ${(!formState.rating && formDisabled) ? 'is-invalid' : ''}`}>
-                  <legend className="rate__caption">Рейтинг
-                    <svg width="9" height="9" aria-hidden="true">
-                      <use xlinkHref="#icon-snowflake"></use>
-                    </svg>
-                  </legend>
-                  <div className="rate__bar">
-                    <div className="rate__group">
-                      <input className="visually-hidden" id="star-5" name="rate" type="radio" value="5" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
-                      <label className="rate__label" htmlFor="star-5" title="Отлично"></label>
-                      <input className="visually-hidden" id="star-4" name="rate" type="radio" value="4" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
-                      <label className="rate__label" htmlFor="star-4" title="Хорошо"></label>
-                      <input className="visually-hidden" id="star-3" name="rate" type="radio" value="3" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
-                      <label className="rate__label" htmlFor="star-3" title="Нормально"></label>
-                      <input className="visually-hidden" id="star-2" name="rate" type="radio" value="2" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
-                      <label className="rate__label" htmlFor="star-2" title="Плохо"></label>
-                      <input className="visually-hidden" id="star-1" name="rate" type="radio" value="1" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
-                      <label className="rate__label" htmlFor="star-1" title="Ужасно"></label>
+    <FocusTrap>
+      <div className="modal is-active">
+        <div className="modal__wrapper" onKeyDown={()=>handleCloseModal}>
+          <div className="modal__overlay" onClick={handleCloseModal}></div>
+          <div className="modal__content">
+            <p className="title title--h4">Оставить отзыв</p>
+            <div className="form-review">
+              <form method="post" onSubmit={handleFormSubmit}>
+                <div className="form-review__rate">
+                  <fieldset className={`rate form-review__item ${(!formState.rating && formDisabled) ? 'is-invalid' : ''}`}>
+                    <legend className="rate__caption">Рейтинг
+                      <svg width="9" height="9" aria-hidden="true">
+                        <use xlinkHref="#icon-snowflake"></use>
+                      </svg>
+                    </legend>
+                    <div className="rate__bar">
+                      <div className="rate__group">
+                        <input className="visually-hidden" id="star-5" name="rate" type="radio" value="5" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
+                        <label className="rate__label" htmlFor="star-5" title="Отлично"></label>
+                        <input className="visually-hidden" id="star-4" name="rate" type="radio" value="4" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
+                        <label className="rate__label" htmlFor="star-4" title="Хорошо"></label>
+                        <input className="visually-hidden" id="star-3" name="rate" type="radio" value="3" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
+                        <label className="rate__label" htmlFor="star-3" title="Нормально"></label>
+                        <input className="visually-hidden" id="star-2" name="rate" type="radio" value="2" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
+                        <label className="rate__label" htmlFor="star-2" title="Плохо"></label>
+                        <input className="visually-hidden" id="star-1" name="rate" type="radio" value="1" onChange={(evt) => setFormState({...formState, rating: +evt.currentTarget.value})} />
+                        <label className="rate__label" htmlFor="star-1" title="Ужасно"></label>
+                      </div>
+                      <div className="rate__progress"><span className="rate__stars">{formState.rating}</span> <span>/</span> <span className="rate__all-stars">5</span>
+                      </div>
                     </div>
-                    <div className="rate__progress"><span className="rate__stars">{formState.rating}</span> <span>/</span> <span className="rate__all-stars">5</span>
-                    </div>
+                    <p className="rate__message">Нужно оценить товар</p>
+                  </fieldset>
+                  <div className={`custom-input form-review__item ${(!formState.userName && formDisabled) ? 'is-invalid' : ''}`}>
+                    <label>
+                      <span className="custom-input__label">Ваше имя
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input type="text" name="user-name" placeholder="Введите ваше имя" onChange={(evt) => setFormState({...formState, userName: evt.currentTarget.value})} />
+                    </label>
+                    <p className="custom-input__error">Нужно указать имя</p>
                   </div>
-                  <p className="rate__message">Нужно оценить товар</p>
-                </fieldset>
-                <div className={`custom-input form-review__item ${(!formState.userName && formDisabled) ? 'is-invalid' : ''}`}>
-                  <label>
-                    <span className="custom-input__label">Ваше имя
-                      <svg width="9" height="9" aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake"></use>
-                      </svg>
-                    </span>
-                    <input type="text" name="user-name" placeholder="Введите ваше имя" onChange={(evt) => setFormState({...formState, userName: evt.currentTarget.value})} />
-                  </label>
-                  <p className="custom-input__error">Нужно указать имя</p>
+                  <div className={`custom-input form-review__item ${(!formState.advantage && formDisabled) ? 'is-invalid' : ''}`}>
+                    <label>
+                      <span className="custom-input__label">Достоинства
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input type="text" name="user-plus" placeholder="Основные преимущества товара" onChange={(evt) => setFormState({...formState, advantage: evt.currentTarget.value})} />
+                    </label>
+                    <p className="custom-input__error">Нужно указать достоинства</p>
+                  </div>
+                  <div className={`custom-input form-review__item ${(!formState.disadvantage && formDisabled) ? 'is-invalid' : ''}`}>
+                    <label>
+                      <span className="custom-input__label">Недостатки
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <input type="text" name="user-minus" placeholder="Главные недостатки товара" onChange={(evt) => setFormState({...formState, disadvantage: evt.currentTarget.value})} />
+                    </label>
+                    <p className="custom-input__error">Нужно указать недостатки</p>
+                  </div>
+                  <div className={`custom-textarea form-review__item ${(!(formState.review.length >= MIN_REVIEW_LENGTH) && formDisabled) ? 'is-invalid' : ''}`}>
+                    <label>
+                      <span className="custom-textarea__label">Комментарий
+                        <svg width="9" height="9" aria-hidden="true">
+                          <use xlinkHref="#icon-snowflake"></use>
+                        </svg>
+                      </span>
+                      <textarea
+                        name="user-comment"
+                        placeholder="Поделитесь своим опытом покупки"
+                        onChange={(evt) => setFormState({...formState, review: evt.currentTarget.value})}
+                      >
+                      </textarea>
+                    </label>
+                    <div className="custom-textarea__error">Нужно добавить комментарий</div>
+                  </div>
                 </div>
-                <div className={`custom-input form-review__item ${(!formState.advantage && formDisabled) ? 'is-invalid' : ''}`}>
-                  <label>
-                    <span className="custom-input__label">Достоинства
-                      <svg width="9" height="9" aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake"></use>
-                      </svg>
-                    </span>
-                    <input type="text" name="user-plus" placeholder="Основные преимущества товара" onChange={(evt) => setFormState({...formState, advantage: evt.currentTarget.value})} />
-                  </label>
-                  <p className="custom-input__error">Нужно указать достоинства</p>
-                </div>
-                <div className={`custom-input form-review__item ${(!formState.disadvantage && formDisabled) ? 'is-invalid' : ''}`}>
-                  <label>
-                    <span className="custom-input__label">Недостатки
-                      <svg width="9" height="9" aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake"></use>
-                      </svg>
-                    </span>
-                    <input type="text" name="user-minus" placeholder="Главные недостатки товара" onChange={(evt) => setFormState({...formState, disadvantage: evt.currentTarget.value})} />
-                  </label>
-                  <p className="custom-input__error">Нужно указать недостатки</p>
-                </div>
-                <div className={`custom-textarea form-review__item ${(!(formState.review.length >= MIN_REVIEW_LENGTH) && formDisabled) ? 'is-invalid' : ''}`}>
-                  <label>
-                    <span className="custom-textarea__label">Комментарий
-                      <svg width="9" height="9" aria-hidden="true">
-                        <use xlinkHref="#icon-snowflake"></use>
-                      </svg>
-                    </span>
-                    <textarea
-                      name="user-comment"
-                      placeholder="Поделитесь своим опытом покупки"
-                      onChange={(evt) => setFormState({...formState, review: evt.currentTarget.value})}
-                    >
-                    </textarea>
-                  </label>
-                  <div className="custom-textarea__error">Нужно добавить комментарий</div>
-                </div>
-              </div>
-              <button className="btn btn--purple form-review__btn" type="submit">Отправить отзыв</button>
-            </form>
+                <button className="btn btn--purple form-review__btn" type="submit">Отправить отзыв</button>
+              </form>
+            </div>
+            <button className="cross-btn" type="button" aria-label="Закрыть попап" onClick={handleCloseModal}>
+              <svg width="10" height="10" aria-hidden="true">
+                <use xlinkHref="#icon-close"></use>
+              </svg>
+            </button>
           </div>
-          <button className="cross-btn" type="button" aria-label="Закрыть попап" onClick={handleCloseModal}>
-            <svg width="10" height="10" aria-hidden="true">
-              <use xlinkHref="#icon-close"></use>
-            </svg>
-          </button>
         </div>
       </div>
-    </div>
+    </FocusTrap>
   );
 }
 
--- src/components/modal-success-review/modal-success-review.tsx
+++ src/components/modal-success-review/modal-success-review.tsx
@@ -1,3 +1,5 @@
+import FocusTrap from 'focus-trap-react';
+
 type ModalAction = {
     setSuccessPost: (boolean : boolean) => void,
 }
@@ -16,33 +18,35 @@ function ModalSuccessReview({setSuccessPost}: ModalAction) : JSX.Element {
   };
   document.addEventListener('keydown', handlePressEsc);
   return (
-    <div className="modal is-active modal--narrow">
-      <div className="modal__wrapper">
-        <div className="modal__overlay" onClick={handleOnClickSuccess}></div>
-        <div className="modal__content">
-          <p className="title title--h4">Спасибо за отзыв</p>
-          <svg className="modal__icon" width="80" height="78" aria-hidden="true">
-            <use xlinkHref="#icon-review-success"></use>
-          </svg>
-          <div className="modal__buttons">
-            <button className="btn btn--purple modal__btn modal__btn--fit-width"
+    <FocusTrap>
+      <div className="modal is-active modal--narrow">
+        <div className="modal__wrapper">
+          <div className="modal__overlay" onClick={handleOnClickSuccess}></div>
+          <div className="modal__content">
+            <p className="title title--h4">Спасибо за отзыв</p>
+            <svg className="modal__icon" width="80" height="78" aria-hidden="true">
+              <use xlinkHref="#icon-review-success"></use>
+            </svg>
+            <div className="modal__buttons">
+              <button className="btn btn--purple modal__btn modal__btn--fit-width"
+                type="button"
+                onClick={handleOnClickSuccess}
+              >Вернуться к покупкам
+              </button>
+            </div>
+            <button className="cross-btn"
               type="button"
+              aria-label="Закрыть попап"
               onClick={handleOnClickSuccess}
-            >Вернуться к покупкам
+            >
+              <svg width="10" height="10" aria-hidden="true">
+                <use xlinkHref="#icon-close"></use>
+              </svg>
             </button>
           </div>
-          <button className="cross-btn"
-            type="button"
-            aria-label="Закрыть попап"
-            onClick={handleOnClickSuccess}
-          >
-            <svg width="10" height="10" aria-hidden="true">
-              <use xlinkHref="#icon-close"></use>
-            </svg>
-          </button>
         </div>
       </div>
-    </div>
+    </FocusTrap>
   );
 }
 
