c23bce9328b81be1d47d8fd8f5488b1ab8430f66
Kbalod <kbalod@yandex.ru>
2022-10-17 18:27:14 +0300

    доработал скрол и esc

--- src/components/modal-success-review/modal-success-review.tsx
+++ src/components/modal-success-review/modal-success-review.tsx
@@ -27,14 +27,14 @@ function ModalSuccessReview({setSuccessPost}: ModalAction) : JSX.Element {
           <div className="modal__buttons">
             <button className="btn btn--purple modal__btn modal__btn--fit-width"
               type="button"
-              onClick={()=>setSuccessPost(false)}
+              onClick={handleOnClickSuccess}
             >Вернуться к покупкам
             </button>
           </div>
           <button className="cross-btn"
             type="button"
             aria-label="Закрыть попап"
-            onClick={()=>setSuccessPost(false)}
+            onClick={handleOnClickSuccess}
           >
             <svg width="10" height="10" aria-hidden="true">
               <use xlinkHref="#icon-close"></use>
