61652398814c97fe8a1a6eab3ed19e05b60dfaa9
Kbalod <kbalod@yandex.ru>
2022-10-26 23:28:00 +0300

    исправил загрузку промо

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
 import App from './components/app/app';
+import { Provider } from 'react-redux';
 import { store } from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 
